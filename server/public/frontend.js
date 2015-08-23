9789b7b5495b50efc90b12c4ce06fc42224c3670
lieuzhenghong <lieuzhenghong@Lieubook.local>
2015-08-23 21:50:27 +0800

    Fix frontend
    Don’t know if it works, anyhow whack

--- server/public/frontend.js
+++ server/public/frontend.js
@@ -1,3 +1,4 @@
+"use strict";
 //API Method (SEND)
 //Request:
 //Action:”JOIN”,PlayerID:<string>,GameRoom:<string>
@@ -14,12 +15,13 @@
 //Action:”CLASH”,MyBid:<integer>,OpponentBid:<integer>,MyDice:<integer>,OpponentDice:<integer>,MyCoins:<integer>,OpponentCoins:<integer>,MyHealth://<integer>,OpponentHealth:<integer>
 //Action:”END”,Win:<boolean>,NumberOfRounds:<integer>,MyCoins:<integer>,OpponentCoins:<integer>,MyHealth:<integer>,OpponentHealth:<integer>
 
+var last_index = 0;
+
   
 function send_request (action, room_id, bid) { //bid is an optional parameter
-  var action = action;
   var player_id = document.getElementById('player_id').value;
   //console.log(document.getElementById('player_id'));
-  console.log('player ID'+ player_id);
+  //console.log('player ID'+ player_id);
   console.log(action);
   
   if (action === "JOIN") {
@@ -52,29 +54,77 @@ function send_request (action, room_id, bid) { //bid is an optional parameter
   else console.log("error");
 }
   
-function listen_request (room_id, last_index) {
+function listen_request (room_id) {
   var player_id = document.getElementById('player_id').value;
   //API Method (LISTEN)
   //Request:
   //PlayerID:<string>,GameRoom:<string>,LastIndex:<integer>
   
   $.ajax(
-    { url: '/lb/send', 
+    { url: '/lb/listen', 
       method: "POST", 
       data: {
         PlayerID:player_id, 
         GameRoom:room_id,
+        LastIndex: last_index
       },
       dataType:"json",                      
       success: function(data) {
-        var response = JSON.parse(data);
-        console.log('success');
-        console.log(response);
+        //console.log(data);
+        //console.log(data[0].Action);
+
+        console.log(last_index);
+        handle_response(data, room_id);
+        last_index++;
       }
     }
   );
 }
 
+
+function handle_response(data, room_id) {
+  console.log(data);
+  console.log(data.length);
+  var responses = [];
+  
+  for (var i = 0; i < data.length; i++){
+    responses.push(data[i]);
+    if (data[i].Action === 'START') {
+      console.log('Game started.');
+    }
+    //Display dice.
+    else if (data[i].Action === 'ROLLDICE') {
+      //console.log('My die: ' + data[i].MyDice);
+      //console.log('Opponent\'s die: ' + data[i].OpponentDice);
+      //Prompt for a bid. 
+      var player_bid = prompt('Enter your bid', 0);
+      send_request ('BID', room_id, player_bid);
+    }
+    else if (data[i].Action === 'CLASH') {
+    console.log('Clashing!');
+    alert('Your Bid: ' + data[i].MyBid + 'Opponent Bid: ' + data[i].OpponentBid);
+    }
+    
+    else if (data[i].Action === 'END') {
+      console.log('End phase.');
+    }
+    updateDisplay(data, i);
+  }
+  //Send another listener.
+  //listen_request(room_id);
+}
+
+
+function updateDisplay(data, i) {
+  var info_board = document.getElementById('info_board');
+  var list = (info_board.getElementsByTagName('li'));
+  //console.log(data[i]);
+  list.my_coins.innerHTML ='My Coins: ' + data[i].MyCoins;
+  list.my_die.innerHTML = 'My Dice:' + data[i].MyDice;
+  list.opp_die.innerHTML = 'Opponent\'s Dice: ' + data[i].OpponentDice;
+  list.my_health.innerHTML = 'My Health: ' + data[i].MyHealth;
+  list.opp_health.innerHTML = 'Opponent\'s Health: ' + data[i].OpponentHealth;
+}
   
   
   //var params = 'PlayerID=' + player_id + '&GameRoom=' + room_id + '&LastIndex=' + last_index;
