a2fde8c4762ec8e189f1e4a413e5363dd86d140c
Bernard Teo <btzy1996@hotmail.com>
2015-08-23 22:58:10 +0800

    Initial Text Client

--- server/textclient/textclient.js
+++ server/textclient/textclient.js
@@ -1,105 +1,194 @@
-//API Method (SEND)
-//Request:
-//Action:”JOIN”,PlayerID:<string>,GameRoom:<string>
-//Action:”BID”,PlayerID:<string>,GameRoom:<string>,Bid:<integer>
-
-
-//API Method (LISTEN)
-//Request:
-//PlayerID:<string>,GameRoom:<string>,LastIndex:<integer>
-  
-//Response(array):
-//Action:”START”,MyCoins:<integer>,OpponentCoins:<integer>,MyHealth:<integer>,OpponentHealth:<integer>
-//Action:”ROLLDICE”,MyDice:<integer>,RoundNumber:<integer>,OpponentDice:<integer>,MyCoins:<integer>,OpponentCoins:<integer>,MyHealth://<integer>,OpponentHealth:<integer>
-//Action:”CLASH”,MyBid:<integer>,OpponentBid:<integer>,MyDice:<integer>,OpponentDice:<integer>,MyCoins:<integer>,OpponentCoins:<integer>,MyHealth://<integer>,OpponentHealth:<integer>
-//Action:”END”,Win:<boolean>,NumberOfRounds:<integer>,MyCoins:<integer>,OpponentCoins:<integer>,MyHealth:<integer>,OpponentHealth:<integer>
-
-  
-function send_request (action, room_id, bid) { //bid is an optional parameter
-  var action = action;
-  var player_id = document.getElementById('player_id').value;
-  //console.log(document.getElementById('player_id'));
-  console.log('player ID'+ player_id);
-  console.log(action);
-  
-  if (action === "JOIN") {
-    $.ajax(
-    { url: '/lb/send', 
-      method: "POST", 
-      data: {
-        Action:action,
-        PlayerID:player_id, 
-        GameRoom:room_id
-      },
-      dataType:"json",                      
-      success:function(data){console.log('response received');}
+$(document).ready(function(){
+    var _enable_handler=null;
+    var _enable_backhandler=null;
+    $("#inputbox").keydown(function(e){
+        if(e.which===13){
+            if(_enable_handler){
+                _enable_handler($("#inputbox").val());
+            }
+        }
+        else if(e.which===8){
+            if(_enable_backhandler){
+                _enable_backhandler();
+            }
+        }
     });
-  }
-  else if (action === "BID") {
-    $.ajax(
-    { url: '/lb/send', 
-      method: "POST", 
-      data: {
-        Action:action,
-        PlayerID:player_id, 
-        GameRoom:room_id,
-        Bid:bid
-      },
-      dataType:"json",                      
-      success:function(data){console.log('response received');}
-    });
-  }
-  else console.log("error");
-}
-  
-function listen_request (room_id, last_index) {
-  var player_id = document.getElementById('player_id').value;
-  //API Method (LISTEN)
-  //Request:
-  //PlayerID:<string>,GameRoom:<string>,LastIndex:<integer>
-  
-  $.ajax(
-    { url: '/lb/send', 
-      method: "POST", 
-      data: {
-        PlayerID:player_id, 
-        GameRoom:room_id,
-      },
-      dataType:"json",                      
-      success: function(data) {
-        var response = JSON.parse(data);
-        console.log('success');
-        console.log(response);
-      }
+    var output=function(message){
+        $("#displaybox").append("<br>"+message);
+        $("#displaybox").scrollTop($("#displaybox").prop("scrollHeight"));
     }
-  );
-}
-
-  
-  
-  //var params = 'PlayerID=' + player_id + '&GameRoom=' + room_id + '&LastIndex=' + last_index;
-  
-  //var req = new XMLHttpRequest();
-  //req.open ('POST', '/lb/listen', true);
-  //req.send (params);
-  
-  //Create a new response_listener to listen for the request
-  //var listener = new response_listener(req);
-  
-/*function response_listener (req) {
-  //Use the previous XMLHttpRequest in the outer scope
-  req.onreadystatechange = function () {
-    if (req.readyState == 4 && req.status == 200) {
-      response = req.responseText;
-      console.log(response);
-      //Pass the response to a handle_response function
-      handle_response(response);
-    } 
-  }
-}
-
-function handle_response (response) {
-  var response = JSON.parse(response);
-  //if response.Action.
-}*/
-
+    var disable=function(){
+        $("#inputbox").prop("disabled",true);
+        $("#inputbox").val("");
+        _enable_handler=null;
+        _enable_backhandler=null;
+    }
+    var enable=function(handler,backhandler){
+        $("#inputbox").prop("disabled",false);
+        $("#inputbox").val("");
+        _enable_handler=handler;
+        if(backhandler)_enable_backhandler=backhandler;
+    }
+    for(var i=0;i<100;++i){
+        output("");
+    }
+    $("#inputbox").focus();
+    var _listen_data=[];
+    var _listen_callbacks=[];
+    var _listen_index=0;
+    var _listen_start=false;
+    var _despatch_callback=function(){
+        if(_listen_index<_listen_callbacks.length&&_listen_index<_listen_data.length){
+            _listen_callbacks[_listen_index](_listen_data[_listen_index]);
+            ++_listen_index;
+            _despatch_callback();
+        }
+    }
+    var start_listener=function(playerid,gameroom){
+        _listen_data=[];
+        _listen_callbacks=[];
+        var index=0;
+        _listen_index=0;
+        _listen_start=true;
+        var _listen=function(){
+            $.post(window.location.host+"/lb/listen",{PlayerID:playerid,GameRoom:gameroom,LastIndex:index},function(data){
+                _listen_data.push.apply(_listen_data,data); // concats array
+                index=_listen_data.length;
+                if(_listen_start){
+                    _despatch_callback();
+                    _listen();
+                }
+            },"json");
+        }
+        _listen();
+    };
+    var stop_listener=function(){
+        _listen_start=false;
+    }
+    var listen=function(callback){
+        _listen_callbacks.push(callback);
+        _despatch_callback();
+    }
+    var end_game=function(playerid,message){
+        output("Game ended. "+(message.Win)?"Congratulations, you have won!":"You have lost.");
+        output("Your Coins: "+message.MyCoins+" Opponent Coins: "+message.OpponentCoins+" Your Health: "+message.MyHealth+" Opponent Health: "+message.OpponentHealth);
+        output("Press \"Enter\" to continue...");
+        enable(function(data){
+            disable();
+            prompt_game_room(playerid);
+        });
+    }
+    var play_game_turn=function(playerid,gameroom){
+        listen(function(message){
+            if(message.Action=="END"){
+                return end_game(playerid,message);
+            }
+            if(message.Action=="ROLLDICE"){
+                output("=== Round #"+message.RoundNumber+" ===");
+                output("Your Dice: "+message.MyDice+". Opponent Dice: "+message.OpponentDice+" Your Coins: "+message.MyCoins+" Opponent Coins: "+message.OpponentCoins+" Your Health: "+message.MyHealth+" Opponent Health: "+message.OpponentHealth);
+                output("Please enter your bid:");
+                enable(function(data){
+                    if(parseInt(data,10).toString()==data){
+                        disable();
+                        data=parseInt(data,10);
+                        if(data>=0&&data<=message.MyCoins){
+                            output("You bidded "+data+" coins.");
+                            $.post(window.location.host+"/lb/send",{Action:"BID",PlayerID:playerid,GameRoom:gameroom,Bid:data},function(data){
+                                if(data==="Command OK."){
+                                    output("Awaiting opponent...");
+                                    listen(function(message){
+                                        if(message.Action=="CLASH"){
+                                            output("Opponent bidded "+message.OpponentBid+" coins.");
+                                            if(message.MyBid>message.OpponentBid){
+                                                output("You win this round :)");
+                                            }
+                                            else if(message.MyBid<message.OpponentBid){
+                                                output("You lose this round :(");
+                                            }
+                                            else{
+                                                output("This round is a draw.");
+                                            }
+                                            play_game_turn(playerid,gameroom);
+                                        }
+                                        else{
+                                            output("Server error. Please try another game room.");
+                                            stop_listener();
+                                            prompt_game_room(playerid);
+                                        }
+                                    });
+                                }
+                                  else{
+                                    output("Server error. Please try another game room.");
+                                    stop_listener();
+                                    prompt_game_room(playerid);
+                                }
+                            },"text");
+                        }
+                    }
+                });
+            }
+            else{
+                output("Server error. Server responded with the following:");
+                output(JSON.stringify(message));
+                stop_listener();
+                prompt_game_room(playerid);
+            }
+        });
+    }
+    var start_game=function(playerid,gameroom){
+        $.post(window.location.host+"/lb/send",{Action:"JOIN",PlayerID:playerid,GameRoom:gameroom},function(data){
+            if(data==="Command OK."){
+                output("Joined game at room \""+gameroom+"\".");
+                output("Awaiting opponent...");
+                start_listener();
+                listen(function(message){
+                    if(message.Action=="START"){
+                        output("Game started.");
+                        play_game_turn(playerid,gameroom);
+                    }
+                    else{
+                        output("Server error. Please try another game room.");
+                        stop_listener();
+                        prompt_game_room(playerid);
+                    }
+                });
+            }
+            else{
+                output("Server error. Server responded with the following:");
+                output(data);
+                prompt_game_room(playerid);
+            }
+        },"text");
+    }
+    var prompt_game_room=function(playerid){
+        output("Please enter game room: [ press backspace to log out ]");
+        enable(function(data){
+            if(data){
+                disable();
+                start_game(playerid,data);
+            }
+        },function(){
+            localStorage.removeItem("PlayerID");
+            output("Successfully logged out.");
+            prompt_welcome();
+        });
+    }
+    var prompt_welcome=function(){
+        if(!localStorage.getItem("PlayerID")){
+            output("Welcome! Please enter your name:");
+            enable(function(data){
+                if(data){
+                    disable();
+                    localStorage.setItem("PlayerID",data);
+                    prompt_welcome();
+                }
+            });
+        }
+        else{
+            output("Welcome, "+localStorage.getItem("PlayerID")+"!");
+            prompt_game_room(localStorage.getItem("PlayerID"));
+        }
+    }
+    output("===== Liarbones Text Client =====");
+    prompt_welcome();
+});
